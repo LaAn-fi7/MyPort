4fe108f47470104f68f1714287c1235d7e6818c0
LaAn-fi7 <72112448+LaAn-fi7@users.noreply.github.com>
2025-07-22 15:07:42 +0700

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -1,4 +1,4 @@
 const certificates = [
-  { file: "cert1.png", title: "Sertifikat Dicoding Python" },
+  { file: "s_intro_to_network.jpg", title: "Sertifikat Introduction to Network" },
   { file: "badge1.png", title: "Badge IT Support - Credly" }
 ]
