26357ffd116ca3bf170234fb706381ebba62a6ec
LaAn-fi7 <72112448+LaAn-fi7@users.noreply.github.com>
2025-07-22 18:42:48 +0700

    Create badges.js

---
+++ badges.js
@@ -0,0 +1,5 @@
+const badges = [
+  { file: "b_intro_to_cyber.png", title: "Introduction to Cybersecurity" },
+  { file: "b_intro_to_network.png", title: "Introduction to Network" },
+  { file: "b_intro_to_iot.png", title: "Introduction to IoT and Digital Transformation" }
+]
